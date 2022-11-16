2c8a78ab075025252620a16360d37ede6000c1e0
kingking1223 <kingking1223@gmail.com>
2022-11-16 18:09:55 +0800

    Completed login page

---
+++ login_script.js
@@ -0,0 +1,32 @@
+function changeEyeIcon(){
+    const pwd = document.getElementById("passwordInput");
+    const eye = document.getElementById("hide1");
+    const eyeSlash = document.getElementById("hide2");
+
+    if (pwd.type === 'password'){
+        pwd.type = 'text';
+        eye.style.display = "block";
+        eyeSlash.style.display = "none";  
+    } else {
+        pwd.type = 'password';
+        eye.style.display = "none";
+        eyeSlash.style.display = "block";
+    }
+}
+
+function login(){
+    const username = document.getElementById("usernameInput").value;
+    const password = document.getElementById("passwordInput").value;
+
+    console.log(username)
+    console.log(typeof username)
+    console.log(password)
+    console.log(typeof password)
+
+    if (username == "test" && password == "test2") {
+        alert("1");
+        return false;
+    } else {
+        alert("0");
+    }
+}
